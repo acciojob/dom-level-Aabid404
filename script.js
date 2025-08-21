c453e05897bd34d22340076f408fb095c8511155
AABID DEVELOPER <150765769+Aabid404@users.noreply.github.com>
2025-08-21 10:12:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+window.onload = function() {
+	
+	let element = document.getElementByID("level");
+	let level = 0;
+	
+	while (element) {
+		level ++;
+		element = element.parentElement;
+	}
+
+	alert("The level of the elememt is:" +level);
+}
